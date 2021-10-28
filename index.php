5419c64dcfd0791911ab011c9aabc66cbb745c2e
prateekkarna <39495653+prateekkarna@users.noreply.github.com>
2021-10-28 10:10:24 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <html> 
 	<head> 
-	<title>PHP</title>
+	<title>PHP Prateek</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"/>
 	</head>
 
 <body> 
-	<h1>PHP 2021  Test</h1>
-		<p><b>  Mayank modi Version 11</b></p>
+	<h1>Prateek PHP 2021  Test</h1>
+		<p><b>  Prateek karna Version 11</b></p>
 		<?php echo "The Current Date and yes las  Time is: <br />";
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information VERSION2 </h2> 
